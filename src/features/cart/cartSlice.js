b2b7e6a345096c520b6591d33b2c4429f62e11ee
olivermunkuli <olivermunkuli@outlook.com>
2023-01-23 17:55:42 +0200

    added axios for fetching data

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,26 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 // import cartItems from '../../cartItems';
 
 const url = 'https://course-api.com/react-useReducer-cart-project';
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .catch((err) => console.log(err));
-});
+// export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+//   return fetch(url)
+//     .then((resp) => resp.json())
+//     .catch((err) => console.log(err));
+// });
+
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios(url);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
 
 const initialState = {
   cartItems: [],
@@ -53,7 +66,6 @@ const cartSlice = createSlice({
       state.isLoading = true;
     },
     [getCartItems.fulfilled]: (state, action) => {
-      console.log('action\n', action);
       state.isLoading = false;
       state.cartItems = action.payload;
     },
